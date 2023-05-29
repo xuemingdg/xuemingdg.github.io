46110c6856c1f1765a34cc386d1db0d05cc8dcb2
Xueming <xuemingdg@outlook.com>
2023-05-29 13:55:01 +0800

    use svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
     kit: {
         adapter: adapter({
-            // See below for an explanation of these options
-            routes: {
-                include: ['/*'],
-                exclude: ['<all>']
-            }
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
         })
     }
 };
