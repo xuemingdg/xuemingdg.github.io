8605302e6bc2423a216eba1a5625cfb319ed7d73
Xueming <xuemingdg@outlook.com>
2023-05-29 17:45:16 +0800

    use svelte js

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
-    }
-};
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+}
